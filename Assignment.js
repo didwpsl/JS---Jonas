7d46c39caa76dc7ce8ce385243f4a46224424110
didwpsl <sysy931207@gmail.com>
2021-10-09 17:45:12 +0900

    Data Types

---
+++ Assignment.js
@@ -0,0 +1,31 @@
+//Practice Assignment// 
+/* Declare variables called 'country' 'continent' and 'population'
+and assign their values according to your own country (population in millions)*/
+
+let country = 'Korea';
+let continent = 'Asia';
+let population = '50m';
+
+//Log their values to the console// 
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+/* 1. Declare a variable called 'isIsland' and set its value according to your
+country. The variable should hold a Boolean value. Also declare a variable
+'language', but don't assign it any value yet. */
+
+let isIsland = false; 
+let language
+
+/* 2. Log the types of 'isLsland', 'population', 'country' and 'language'
+to the console. */
+
+console.log(isIsland);
+console.log(language);
+console.log(population);
+console.log(country);
+
+console.log(`I am from ${country} in ${continent} My country's population is ${population} 
+and we speak ${language}`);
---
+++ script01.js
@@ -0,0 +1,31 @@
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof javaScriptIsFun);
+console.log(typeof 2222);
+
+let a = 'Hi';
+let b = "Hello";
+
+console.log(a, b);
+
+javaScriptIsFun = 'YES!';
+console.log(typeof javaScriptIsFun);
+javaScriptIsFun = 23;
+console.log(javaScriptIsFun);
+console.log(typeof javaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+year = 'lol'
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
