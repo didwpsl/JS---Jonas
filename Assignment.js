4cea92f3bf204d6eab9746d29e9ecb7fb39cf249
didwpsl <sysy931207@gmail.com>
2021-10-09 18:50:11 +0900

    Basic Operators

--- Assignment.js
+++ Assignment.js
@@ -36,7 +36,7 @@ and we speak ${language}`);
 /* 1. Set the value of 'language' to the language spoken where you live (some
     countries have multiple languages, but just choose one)*/
 
-language = 'KOREAN';
+/*language = 'KOREAN';
 console.log(language);
 
 /* 2.Think about which variables should be const variables (which values will never 
@@ -44,14 +44,43 @@ console.log(language);
 
 //A. changeable value = population, non changeable = country, countinent, language, isIsland//
 
-const country = "KOREA";
+/*const country = "KOREA";
 console.log(country);
 let population = "50m";
 console.log(population);
 
 /* 3.Try to change one of the changed variables now, and observe what happens */
 
-population = "40m";
+/*population = "40m";
 console.log(population);
 country = 'AMERICA';
 console.log(country);
+
+//Basic Operators// 
+/* 1. If your country split in half, and each half would contain half the population,
+then how many people would live in each half?*/
+let population = '50000000';
+const country = 'KOREA';
+const continent = 'ASIA';
+const language = 'KOREAN';
+console.log(population / 2 );
+
+
+/* 2. Increase the population of your country by 1 and log the result to the console */
+
+console.log(++population);
+
+/* 3.Finland has a population of 6 million. Does your country have more people than
+Finland?*/
+console.log(population > 60000000);
+
+/* 4.The average population of a country is 33 million people. Does your country
+have less people than the average country? */
+console.log(population < 33000000);
+
+/* 5.
+Based on the variables you created, create a new variable 'description'
+which contains a string with this format: 'Portugal is in Europe, and its 11 million
+people speak portuguese */ 
+const description1 = `${country} is in ${continent}, and its ${population} people speak ${language}`;
+console.log(description1);
--- script01.js
+++ script01.js
@@ -13,3 +13,32 @@ console.log(job);
 
 myName = 'SOYOUNG';
 console.log(myName); //avoid declaring variable without var or const or let
+
+const now = 2000;
+const ageJenny = now - 1993;
+const ageJohn = now - 1990;
+console.log(ageJenny);
+console.log(ageJohn);
+
+console.log(ageJenny * 2, ageJenny**3);
+
+const firstName = 'SOYOUNG'
+const lastName = 'YANG'
+console.log(firstName + " " + lastName);
+
+let x = 10 + 5;
+x += 10;
+x *= 4;
+x ++ ;
+x--
+x--
+console.log(x);
+
+console.log(ageJenny < ageJohn);
+console.log(ageJenny <=18);
+console.log(ageJenny >= 100);
+
+const isFullAge = ageJenny >= 100;
+console.log(isFullAge);
+
+console.log(now - 1991 > now - 2020);
