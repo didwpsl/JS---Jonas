5982db29e136bc3a09ea086e3f7bc006df9d180b
didwpsl <sysy931207@gmail.com>
2021-10-10 17:09:21 +0900

    Assignment by Boolean Logic

--- Assignment.js
+++ Assignment.js
@@ -1,86 +1,165 @@
-//Practice Assignment// 
-//values and Variables// 
-/* Declare variables called 'country' 'continent' and 'population'
-and assign their values according to your own country (population in millions)*/
+/*
+LECTURE: Strings and Template Literals
 
-/*let country = 'Korea';
-let continent = 'Asia';
-let population = '50m';
-
-//Log their values to the console// 
-
-console.log(country);
-console.log(continent);
-console.log(population);
-
-//Data Types// 
-/* 1. Declare a variable called 'isIsland' and set its value according to your
-country. The variable should hold a Boolean value. Also declare a variable
-'language', but don't assign it any value yet. */
-
-/*let isIsland = false; 
-let language
-
-/* 2. Log the types of 'isLsland', 'population', 'country' and 'language'
-to the console. */
-
-/*console.log(isIsland);
-console.log(language);
-console.log(population);
-console.log(country);
-
-console.log(`I am from ${country} in ${continent} My country's population is ${population} 
-and we speak ${language}`);
-
-//let, const and var//
-/* 1. Set the value of 'language' to the language spoken where you live (some
-    countries have multiple languages, but just choose one)*/
-
-/*language = 'KOREAN';
-console.log(language);
-
-/* 2.Think about which variables should be const variables (which values will never 
-    change, and which might change?) Then, change these variables to const */
-
-//A. changeable value = population, non changeable = country, countinent, language, isIsland//
-
-/*const country = "KOREA";
-console.log(country);
-let population = "50m";
-console.log(population);
-
-/* 3.Try to change one of the changed variables now, and observe what happens */
-
-/*population = "40m";
-console.log(population);
-country = 'AMERICA';
-console.log(country);
-
-//Basic Operators// 
-/* 1. If your country split in half, and each half would contain half the population,
-then how many people would live in each half?*/
-let population = '50000000';
+1. Recreate the 'description' variable from the last assignment, this time
+using the template literal syntax
+*/
+/*let population = '50000000';
 const country = 'KOREA';
 const continent = 'ASIA';
 const language = 'KOREAN';
-console.log(population / 2 );
 
-
-/* 2. Increase the population of your country by 1 and log the result to the console */
-
-console.log(++population);
-
-/* 3.Finland has a population of 6 million. Does your country have more people than
-Finland?*/
-console.log(population > 60000000);
-
-/* 4.The average population of a country is 33 million people. Does your country
-have less people than the average country? */
-console.log(population < 33000000);
-
-/* 5.
-Based on the variables you created, create a new variable 'description'
-which contains a string with this format: 'Portugal is in Europe, and its 11 million
-people speak portuguese */ 
 const description1 = `${country} is in ${continent}, and its ${population} people speak ${language}`;
-console.log(description1);
+
+const description = "Korea is in Asia, and its 50millions people speak Korean";
+console.log(description);
+
+/*
+LECTURE: Taking Decisions: if / else Statements
+
+1. If your country's population is greater that 33 million, log a string like this to the
+console: 'Portugal's population is above average'. Otherwise, log a string like
+'Portugal's population is 22 million below average' (the 22 is the average of 33
+minus the country's population)
+*/
+/*
+if (population > 33000000){
+    console.log("Korea's population is above average");
+} else console.log("Korea's population is 22million below average");
+
+/*
+2. After checking the result, change the population temporarily to 13 and then to
+130. See the different results, and set the population back to original
+*/
+/*
+population = 13000000;
+if (population > 33000000){
+    console.log("Korea's population is above average");
+} else console.log("Korea's population is 22million below average");
+
+population = 130000000;
+if (population > 33000000){
+    console.log("Korea's population is above average");
+} else console.log("Korea's population is 22million below average");
+
+population = 50000000;
+
+/*LECTURE: Type Conversion and Coercion
+
+1. Predict the result of these 5 operations without executing them:
+'9' - '5';
+'19' - '13' + '17';
+'19' - '13' + 17;
+'123' < 57;
+5 + 6 + '4' + 9 - 4 - 2;
+*/
+//my expectation 4, 617, 23, false, 1143 
+
+/*
+2. Execute the operations to check if you were right
+*/
+console.log('9' - '5');
+console.log('19' - '13' + '17');
+console.log('19' - '13' + 17);
+console.log('123' < 57);
+console.log(5 + 6 + '4' +9 -4 -2);
+
+/*
+LECTURE: Equality Operators: == vs. ===
+
+1. Declare a variable 'numNeighbours' based on a prompt input like this:
+prompt('How many neighbour countries does your country
+have?');
+*/
+/*
+const numNeighbours = prompt('How many neighbour countries does your country have?');
+
+/*
+2. If there is only 1 neighbour, log to the console 'Only 1 border!' (use loose equality
+== for now)
+*/
+//if (numNeighbours == 1) {console.log('Only 1 border!');}
+
+/*
+3. Use an else-if block to log 'More than 1 border' in case 'numNeighbours'
+is greater than 1
+*/
+//else if(numNeighbours > 1) {console.log('More than 1 border');}
+
+/*
+4. Use an else block to log 'No borders' (this block will be executed when
+'numNeighbours' is 0 or any other value)
+*/
+//else console.log('No borders');
+/*
+5. Test the code with different values of 'numNeighbours', including 1 and 0.
+*/
+
+/*if (numNeighbours == 1) {console.log('Only 1 border!');}
+else if(numNeighbours > 1) {console.log('More than 1 border');}
+else console.log('No borders');*/
+
+/*
+6. Change == to ===, and test the code again, with the same values of
+'numNeighbours'. Notice what happens when there is exactly 1 border! Why
+is this happening?
+*/
+/*
+if (numNeighbours === 1) {
+    console.log('Only 1 border!');
+}
+    else if(numNeighbours > 1) {
+        console.log('More than 1 border');
+    }
+    else console.log('No borders');
+*/
+/*
+7. Finally, convert 'numNeighbours' to a number, and watch what happens now
+when you input 1
+*/
+/*
+if (Number(numNeighbours) === 1) {
+    console.log('Only 1 border!');
+}
+    else if(numNeighbours > 1) {
+        console.log('More than 1 border');
+    }
+    else console.log('No borders');
+
+    /*
+ LECTURE: Logical Operators
+1. Comment out the previous code so the prompt doesn't get in the way
+*/
+
+/*
+2. Let's say Sarah is looking for a new country to live in. She wants to live in a
+country that speaks english, has less than 50 million people and is not an
+island.
+*/
+let population = 50000000;
+const country = 'KOREA';
+const continent = 'ASIA';
+const language = 'KOREAN';
+
+/*
+3. Write an if statement to help Sarah figure out if your country is right for her.
+You will need to write a condition that accounts for all of Sarah's criteria. Take
+your time with this, and check part of the solution if necessary.
+*/
+const counSarah = (language === "English" && population < 50000000 && continent !== "ISLAND")
+
+/*
+4. If yours is the right country, log a string like this: 'You should live in Portugal :)'. If
+not, log 'Portugal does not meet your criteria :('
+*/
+
+if (counSarah == true){
+    console.log('You should live in Korea');
+} 
+else console.log('Korea does not meet your criteria');
+
+/*
+5. Probably your country does not meet all the criteria. So go back and temporarily
+change some variables in order to make the condition true (unless you live in
+Canada :D)
+*/
