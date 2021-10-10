5d1886cea20441c5129fd4e6d9f3eb56e978cb2f
didwpsl <sysy931207@gmail.com>
2021-10-10 15:12:30 +0900

    Type Conversion and Coercion

---
+++ script01.js
@@ -0,0 +1,24 @@
+//Type conversion // 
+
+const inputYear = '1991';
+console.log(Number(inputYear));
+console.log(Number(inputYear) + 18);
+
+let Jenny = '제니';
+console.log(Number(Jenny));
+console.log(typeof NaN);
+
+console.log(String(23)+18);
+
+//Type coercion // 
+console.log('I am ' + 23 + ' years old');
+
+console.log('23' - '10' -3);
+
+console.log('23' / '2');
+
+console.log('23' > '18');
+
+let n = '1' + 1;
+n = n - 1;
+console.log(n);
