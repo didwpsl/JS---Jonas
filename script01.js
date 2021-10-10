8b2c3b820842394e677ab4b0509d9e3e6059eb67
didwpsl <sysy931207@gmail.com>
2021-10-10 15:25:20 +0900

    Truthy and Falsy Values

--- script01.js
+++ script01.js
@@ -1,24 +1,21 @@
-//Type conversion // 
+//5 falsy values: 0, '', undefined, null, NaN // 
 
-const inputYear = '1991';
-console.log(Number(inputYear));
-console.log(Number(inputYear) + 18);
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean(' '));
 
-let Jenny = '제니';
-console.log(Number(Jenny));
-console.log(typeof NaN);
+const money = 0;
+if(money) {
+    console.log("Don't spend it all");
+} else {
+    console.log('You should get a job')
+;}
 
-console.log(String(23)+18);
-
-//Type coercion // 
-console.log('I am ' + 23 + ' years old');
-
-console.log('23' - '10' -3);
-
-console.log('23' / '2');
-
-console.log('23' > '18');
-
-let n = '1' + 1;
-n = n - 1;
-console.log(n);
+let height;
+if(height){
+    console.log('YAY! Height is defined');
+} else {
+    console.log('Height is UNDEFINED.');
+}
