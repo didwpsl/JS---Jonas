c8d10c60575dd20f8c8671c2d6414669d27416fc
didwpsl <sysy931207@gmail.com>
2021-10-11 20:29:03 +0900

    Introduction to Arrays

--- script01.js
+++ script01.js
@@ -1,19 +1,42 @@
-const calAge = function (birthYear){
+const friend1 = 'Michael';
+const friend2 = 'Steven';
+const friend3 = 'Peter'; 
+
+
+//more usual 
+const friends = ['Michel', 'Steven', 'Peter'];
+console.log(friends);
+
+/*
+const years = new Array(1991, 1984, 2008, 2020);
+console.log(years);
+*/
+console.log(friends[0]); //Arrays always start from 0
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = 'Jay';
+console.log(friends[2]);
+
+const firstName = 'Soyoung';
+const soyoung = [firstName, 'Yang', 2021-1993, 'teacher', friends];
+console.log(soyoung);
+console.log(soyoung.length);
+
+const calcAge = function (birthYear){
     return 2021 - birthYear;
 }
+const years = [1990, 1967, 2010, 2002, 2018];
 
-const yearUntilRetirement = function (birthYear, firstName) {
-    const age = calAge(birthYear);
-    const retirement = 65 - age; 
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[2]);
+const age4 = calcAge(years[3]);
+const age5 = calcAge(years[4]);
 
-    if (retirement > 0) {
-        console.log(`${firstName} retires in ${retirement} years`);
-        return retirement;
-    } else {
-        console.log(`${firstName} has already retired`);
-        return -1; 
-    }
+console.log(age1, age2, age3, age4, age5);
 
-}
-
-console.log(yearUntilRetirement(1991, 'Jonas'));
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[2]), calcAge(years[years.length - 1])];
+console.log(ages);
