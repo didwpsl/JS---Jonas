f8fcc2c7f879e52b568a05b2180c7d813556f88d
didwpsl <sysy931207@gmail.com>
2021-10-11 20:43:09 +0900

    Assignment Array

--- Assignment.js
+++ Assignment.js
@@ -291,3 +291,37 @@ function describePopulation (country, population){
 console.log(describePopulation('Korea', 50000000));
 console.log(describePopulation('Japan', 1200000000));
 console.log(describePopulation('Australia', 20000000));
+/*
+LECTURE: Introduction to Arrays
+
+1. Create an array containing 4 population values of 4 countries of your choice.
+You may use the values you have been using previously. Store this array into a
+variable called 'populations'
+*/
+const popKorea = 50000000;
+const popJapan = 12000000000;
+const popAustralia = 20000000;
+const popPhilippines = 100000000;
+const capKorea = 'Seoul';
+const capJapan = 'Tokyo';
+const capAustralia = 'Canberra';
+const lanKorea = 'Korean';
+const lanJapan = 'Japanese';
+const lanAustralia = 'English';
+
+const populations = [popKorea, popJapan, popPhilippines, popAustralia];
+
+
+//2. Log to the console whether the array has 4 elements or not (true or false)
+
+console.log(populations.length > 3);
+
+/*
+3. Create an array called 'percentages' containing the percentages of the
+world population for these 4 population values. Use the function
+'percentageOfWorld1' that you created earlier to compute the 4
+percentage values
+*/
+
+const percentages = [percentageOfWorld1(populations[0]), percentageOfWorld1(populations[1]), percentageOfWorld1(populations[2]), percentageOfWorld1(populations[3])];
+console.log(percentages);
