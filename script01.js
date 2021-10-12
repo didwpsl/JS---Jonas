26a091fde656da9d5dab6724c54aba99f869ae5d
didwpsl <sysy931207@gmail.com>
2021-10-12 17:43:59 +0900

    Looping Arrays, Breaking and Continuing

--- script01.js
+++ script01.js
@@ -1,54 +1,18 @@
-
-const jonas = {
-    firstName: 'Jonas',
-    lastName: 'Schmedtmann',
-    birthYear: 1991,
-    job: 'teacher',
-    friends: ['Michael', 'Peter', 'Steven'],
-    hasDriversLicense: true,
-
-
-    calcAge : function (){
-        this.age = 2021 - this.birthYear;
-        return this.age;
-    },
-
-    getSummary : function (){
-        return `${jonas.firstName} is a ${jonas.calcAge()} year old ${jonas.job} and he has a driver's license"`
-    }
-    }
-
-    /*
-    calcAge: function(birthYear){
-        return 2021 - birthYear;
-    }
-};
-
-    calcAge: function(){
-        console.log(this);
-        return 2021 - this.birthYear;*/
-
-console.log(jonas.calcAge());
-console.log(jonas.age);
+/*console.log('Lifting weights repetition 1')
+console.log('Lifting weights repetition 2')
+console.log('Lifting weights repetition 3')
+console.log('Lifting weights repetition 4')
+console.log('Lifting weights repetition 5')
+console.log('Lifting weights repetition 6')
+console.log('Lifting weights repetition 7')
+console.log('Lifting weights repetition 8')
+console.log('Lifting weights repetition 9')
+console.log('Lifting weights repetition 10')
+*/
+// for loop keeps running while condition is TRUE 
+for(let rep = 5; rep <= 30; rep++ ){
+    console.log(`Lifting weights repetition ${rep}`);
+}
 
 
-//console.log(jonas.calcAge(1993))
-
-//console.log(jonas['calcAge'](1993))
-
-//Challenge 
-//"Jonas is a 46-year old teacher and he has a driver's license"
-
-
-if (hasDriversLicense =true) {
-    console.log(`"${jonas.firstName} is a ${jonas.calcAge()} year old ${jonas.job} and he has a driver's license"`);
-}else console.log(`${jonas.firstName} cannot drive`);
-
-
-
-if (hasDriversLicense = true){
-    console.log(jonas.getSummary())
-} else console.log(`${jonas.firstName} cannot drive`);
-
-console.log(jonas.getSummary());
 
