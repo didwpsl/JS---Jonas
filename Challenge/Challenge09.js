3404a19f7658f853ca4ad7694c78bbec20e9e3ec
didwpsl <sysy931207@gmail.com>
2021-10-14 20:59:15 +0900

    Guess my number

--- Challenge/Challenge09.js
+++ Challenge/Challenge09.js
@@ -25,9 +25,8 @@ const printForecast = function(arr){
     let str = ''
     for (let i=0; i < arr.length ; i++){
     str = str + `... ${arr[i]}ºC in ${i + 1}days` 
-    console.log(str);
-
     }
+    console.log(str);
 }
 
 printForecast(degree);
