6a55d1ca476f5a2fb8cb2264af9524af808e2683
didwpsl <sysy931207@gmail.com>
2021-10-19 23:21:27 +0900

    Guess my number 2

--- Projects/script.js
+++ Projects/script.js
@@ -1,4 +1,47 @@
 'use strict';
+/*
+console.log(document.querySelector('.message').
+textContent);
+document.querySelector('.message').textContent = 'Correct Number!🎉';
 
-console.log(document.querySelector('.message').textContent);
+document.querySelector('.number').textContent= 13;
+document.querySelector('.score').textContent = 11; 
 
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
+
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+let score = 5;  
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function(){
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess)
+
+    
+
+        if (guess > secretNumber){
+            if (score > 1){
+            document.querySelector('.message').textContent = 'Too high!';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } 
+        else document.querySelector('.message').textContent = 'YOU LOST!'} 
+
+        else if (guess < secretNumber){
+            if (score > 1){
+            document.querySelector('.message').textContent = 'Too Low!';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } 
+        else document.querySelector('.message').textContent = 'YOU LOST!'} 
+        else if (guess === secretNumber){
+            if (score > 1){
+            document.querySelector('.message').textContent = 'Correct!!';
+            score++;
+            document.querySelector('.score').textContent = score;}
+            else document.querySelector('.message').textContent = 'YOU LOST!';}
+        }
+
+);
